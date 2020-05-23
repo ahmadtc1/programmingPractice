0014c3c0906a61246c4e9576c01fb75361c60c27
ahmadtc1 <ahmadtc2000@gmail.com>
2020-05-23 18:40:57 -0400

    An O(n) solution to the problem seeking to determine whether a given number is a perfect square

---
+++ validPerfectSquare.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if (num == 1) 
+        {
+            return true;
+        }
+        for (unsigned int i = 0; i <= num / 2; ++i) {
+            if ((i * i) == num)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
