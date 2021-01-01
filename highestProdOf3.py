660a4c1e4f28215453c6966686e0d3a2a54d2674
ahmadtc1 <ahmadtc2000@gmail.com>
2021-01-01 17:33:01 -0500

    Finding the highest possible product of 3 fmo a list in O(nlogn) time using O(1) memory

---
+++ highestProdOf3.py
@@ -0,0 +1,60 @@
+import unittest
+
+def highest_product_of_3(list):
+    list = sorted(list)
+
+    if (list[-1] <= 0):
+        return list[-1] * list[-2] * list[-3]
+
+
+    if (list[0] * list[1] > list[-1] * list[-2]):
+        return list[0] * list[1] * list[-1]
+
+    else:
+        return list[-1] * list[-2] * list[-3]
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_short_list(self):
+        actual = highest_product_of_3([1, 2, 3, 4])
+        expected = 24
+        self.assertEqual(actual, expected)
+
+    def test_longer_list(self):
+        actual = highest_product_of_3([6, 1, 3, 5, 7, 8, 2])
+        expected = 336
+        self.assertEqual(actual, expected)
+
+    def test_list_has_one_negative(self):
+        actual = highest_product_of_3([-5, 4, 8, 2, 3])
+        expected = 96
+        self.assertEqual(actual, expected)
+
+    def test_list_has_two_negatives(self):
+        actual = highest_product_of_3([-10, 1, 3, 2, -10])
+        expected = 300
+        self.assertEqual(actual, expected)
+
+    def test_list_is_all_negatives(self):
+        actual = highest_product_of_3([-5, -1, -3, -2])
+        expected = -6
+        self.assertEqual(actual, expected)
+
+    def test_error_with_empty_list(self):
+        with self.assertRaises(Exception):
+            highest_product_of_3([])
+
+    def test_error_with_one_number(self):
+        with self.assertRaises(Exception):
+            highest_product_of_3([1])
+
+    def test_error_with_two_numbers(self):
+        with self.assertRaises(Exception):
+            highest_product_of_3([1, 1])
+
+
+unittest.main(verbosity=2)
